3d8fa95d256138b05f9a887cb29635f7e9ddd07b
Emmanuel <emmanuelfernandez600@gmail.com>
2021-03-16 12:48:11 -0300

    Added CharacterDetails controller

---
+++ GenshinMVC/Controllers/CharacterDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenshinMVC.Controllers
+{
+    public class CharacterDetailsController : Controller
+    {
+        public ActionResult Index(int id = 1)
+        {
+            return View();
+        }
+    }
+}
